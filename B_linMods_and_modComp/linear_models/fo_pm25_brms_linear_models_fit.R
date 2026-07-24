a1f4657918ae70ffbfaed5097b19441001b1ede7
aurberto <berto.aur01@gmail.com>
2026-07-24 17:27:36 +0200

    Add files via upload

---
+++ B_linMods_and_modComp/linear_models/fo_pm25_brms_linear_models_fit.R
@@ -0,0 +1,140 @@
+# ------------------------------------------------------------------------------
+# LEiDA FO prediction from air pollution (K=13; PM2.5)
+# ------------------------------------------------------------------------------
+# Berto A. 05/2026 aurber@utu.fi
+
+# This code will test 3 models: null model, only-pollutant model, and a full 
+# model (pollutant+covariates) with a moderation term between ADI and pollutant.
+# Each model (but the null one) will have site as random effect. 
+# Final model comparison will be done to obtain the best model for following
+# statistical inference.
+
+# This code is adapted to be run in CSC - Puhti, using the script 
+# run_Rscripts.sh from the terminal.
+
+# --- Setup libraries for custom R ---------------------------------------------
+
+user_lib <- Sys.getenv("R_LIBS_USER")
+.libPaths(c(user_lib, .libPaths()[!grepl("r-env", .libPaths())]))
+print(.libPaths())
+Sys.setenv(TMPDIR = "/path/to/ABCD-brms_pollution/tmp")
+
+# --- Parallel options ---------------------------------------------------------
+
+options(mc.cores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")), scipen = 100000)
+
+# --- Load packages ------------------------------------------------------------
+
+library(dplyr)
+library(stringr)
+library(knitr)
+library(brms)
+library(loo)
+library(rlang)
+
+# --- Load data and set paths --------------------------------------------------
+
+data_path <- "/path/to/data/DATASET_analysis_062026.csv"
+data <- read.csv(data_path, header = TRUE, stringsAsFactors = TRUE)
+
+models_path <- "/path/to/results/fractional_occupancy"
+# Create directory if not exist
+if (!dir.exists(models_path)) {
+  dir.create(models_path, recursive = TRUE)
+}
+
+summary_path <- "/path/to/results/fractional_occupancy/model_comparison"
+# Create directory if not exist
+if (!dir.exists(summary_path)) {
+  dir.create(summary_path, recursive = TRUE)
+}
+
+pollutant <- "pm"
+metric <- "fo"
+
+
+# --- Check LEiDA FO distribution ----------------------------------------------
+
+# Check to see if probabilities are outside the range [0,1]
+# and thus need correction before applying the beta-regression model
+
+epsilon <- 1e-4
+data_rescaled <- data %>%
+  mutate(across(starts_with("P_k13c"), 
+                ~ pmin(pmax(., epsilon), 1 - epsilon))) %>%
+  rowwise() %>%
+  mutate(total = sum(c_across(starts_with("P_k13c"))),
+         across(starts_with("P_k13c"), ~ . / total)) %>%
+  ungroup()
+
+
+# --- Null model ---------------------------------------------------------------
+
+fit0 <- brm(
+  formula = cbind(P_k13c1, P_k13c2, P_k13c3, P_k13c4, P_k13c5, P_k13c6, P_k13c7, 
+                  P_k13c8, P_k13c9, P_k13c10, P_k13c11, P_k13c12, P_k13c13) 
+  ~ 1,
+  data = data_rescaled,
+  family = dirichlet,
+  cores = 8, seed = 1
+)
+# fit0
+saveRDS(fit0, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit0_null.RDS")))
+
+
+# --- Model with reshist_addr1_pm25_prenatal_avg -------------------------------
+
+fit1 <- brm(
+  formula = cbind(P_k13c1, P_k13c2, P_k13c3, P_k13c4, P_k13c5, P_k13c6, P_k13c7, 
+                  P_k13c8, P_k13c9, P_k13c10, P_k13c11, P_k13c12, P_k13c13) 
+  ~ reshist_addr1_pm25_prenatal_avg,
+  data = data_rescaled,
+  family = dirichlet,
+  cores = 8, seed = 1
+)
+# fit1
+saveRDS(fit1, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit1_only_prenatal.RDS")))
+
+
+# --- Model with reshist_addr1_pm252016aa --------------------------------------
+
+fit2 <- brm(
+  formula = cbind(P_k13c1, P_k13c2, P_k13c3, P_k13c4, P_k13c5, P_k13c6, P_k13c7, 
+                  P_k13c8, P_k13c9, P_k13c10, P_k13c11, P_k13c12, P_k13c13) 
+  ~ reshist_addr1_pm252016aa,
+  data = data_rescaled,
+  family = dirichlet,
+  cores = 8, seed = 1
+)
+# fit2
+saveRDS(fit2, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit2_only_current.RDS")))
+
+
+# --- Model with reshist_addr1_pm25_prenatal_avg + cov. + mod. -----------------
+
+fit3 <- brm(
+  formula = cbind(P_k13c1, P_k13c2, P_k13c3, P_k13c4, P_k13c5, P_k13c6, P_k13c7, 
+                  P_k13c8, P_k13c9, P_k13c10, P_k13c11, P_k13c12, P_k13c13) 
+  ~ reshist_addr1_pm25_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+    pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg,
+  data = data_rescaled,
+  family = dirichlet,
+  cores = 8, seed = 1
+)
+# fit3
+saveRDS(fit3, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit3_mod_prenatal.RDS")))
+
+
+# --- Model reshist_addr1_pm252016aa + cov. + mod. -----------------------------
+
+fit4 <- brm(
+  formula = cbind(P_k13c1, P_k13c2, P_k13c3, P_k13c4, P_k13c5, P_k13c6, P_k13c7, 
+                  P_k13c8, P_k13c9, P_k13c10, P_k13c11, P_k13c12, P_k13c13) 
+  ~ reshist_addr1_pm252016aa*reshist_addr1_adi_quint + demo_sex_v2 + 
+    pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg,
+  data = data_rescaled,
+  family = dirichlet,
+  cores = 8, seed = 1
+)
+# fit4
+saveRDS(fit4, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit4_mod_current.RDS")))
---
+++ B_linMods_and_modComp/linear_models/power_no2_brms_linear_models_fit.R
@@ -0,0 +1,148 @@
+# ------------------------------------------------------------------------------
+# POWER prediction from air pollution (H6; NO2)
+# ------------------------------------------------------------------------------
+# Berto A. 05/2026 aurber@utu.fi
+
+# This code will test 3 models: null model, only-pollutant model, and a full 
+# model (pollutant+covariates) with a moderation term between ADI and pollutant.
+# Each model (but the null one) will have site as random effect. 
+# Final model comparison will be done to obtain the best model for following
+# statistical inference.
+
+# This code is adapted to be run in CSC - Puhti, using the script 
+# run_Rscripts.sh from the terminal.
+
+# --- Setup libraries for custom R ---------------------------------------------
+user_lib <- Sys.getenv("R_LIBS_USER")
+.libPaths(c(user_lib, .libPaths()[!grepl("r-env", .libPaths())]))
+print(.libPaths())
+Sys.setenv(TMPDIR = "/path/to/ABCD-brms_pollution/tmp")
+
+# --- Parallel options ---------------------------------------------------------
+options(mc.cores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")), scipen = 100000)
+
+# --- Load packages ------------------------------------------------------------
+library(dplyr)
+library(stringr)
+library(knitr)
+library(brms)
+library(loo)
+library(rlang)
+
+# --- Load data and set paths --------------------------------------------------
+data_path <- "/path/to/data/DATASET_analysis_052026.csv"
+data <- read.csv(data_path, header = TRUE, stringsAsFactors = TRUE)
+
+models_path <- "/path/to/results/fch/power"
+# Create directory if not exist
+if (!dir.exists(models_path)) {
+  dir.create(models_path, recursive = TRUE)
+}
+
+summary_path <- "/path/to/results/fch/power/model_comparison"
+# Create directory if not exist
+if (!dir.exists(summary_path)) {
+  dir.create(summary_path, recursive = TRUE)
+}
+
+pollutant <- "no2"
+metric <- "power"
+
+
+# --- Null model ---------------------------------------------------------------
+fit0 <- brm(
+  formula = bf(Harmonics_power1 ~ 1) +
+    bf(Harmonics_power2 ~ 1) +
+    bf(Harmonics_power3 ~ 1) +
+    bf(Harmonics_power4 ~ 1) +
+    bf(Harmonics_power5 ~ 1) +
+    bf(Harmonics_power6 ~ 1),
+  data = data,
+  family = gaussian(),
+  cores = 8,
+  seed = 1
+)
+# fit0
+saveRDS(fit0, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit0_null.RDS")))
+
+
+# --- Model with reshist_addr1_no2_prenatal_avg -------------------------------
+fit1 <- brm(
+  formula = bf(Harmonics_power1 ~ reshist_addr1_no2_prenatal_avg + (1|site)) +
+    bf(Harmonics_power2 ~ reshist_addr1_no2_prenatal_avg + (1|site)) +
+    bf(Harmonics_power3 ~ reshist_addr1_no2_prenatal_avg + (1|site)) +
+    bf(Harmonics_power4 ~ reshist_addr1_no2_prenatal_avg + (1|site)) +
+    bf(Harmonics_power5 ~ reshist_addr1_no2_prenatal_avg + (1|site)) +
+    bf(Harmonics_power6 ~ reshist_addr1_no2_prenatal_avg + (1|site)),
+  data = data,
+  family = gaussian(),
+  cores = 8,
+  seed = 1
+)
+# fit1
+saveRDS(fit1, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit1_only_prenatal.RDS")))
+
+
+# --- Model with reshist_addr1_no2_2016_aavg --------------------------------------
+fit2 <- brm(
+  formula = bf(Harmonics_power1 ~ reshist_addr1_no2_2016_aavg + (1|site)) +
+    bf(Harmonics_power2 ~ reshist_addr1_no2_2016_aavg + (1|site)) +
+    bf(Harmonics_power3 ~ reshist_addr1_no2_2016_aavg + (1|site)) +
+    bf(Harmonics_power4 ~ reshist_addr1_no2_2016_aavg + (1|site)) +
+    bf(Harmonics_power5 ~ reshist_addr1_no2_2016_aavg + (1|site)) +
+    bf(Harmonics_power6 ~ reshist_addr1_no2_2016_aavg + (1|site)),
+  data = data,
+  family = gaussian(),
+  cores = 8,
+  seed = 1
+)
+# fit2
+saveRDS(fit2, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit2_only_current.RDS")))
+
+
+# --- Model with reshist_addr1_no2_prenatal_avg + cov. + mod. -----------------
+
+fit3 <- brm(
+  formula = bf(Harmonics_power1 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+                 pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power2 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power3 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power4 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power5 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power6 ~ reshist_addr1_no2_prenatal_avg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)),
+  data = data,
+  family = gaussian(),
+  cores = 8,
+  seed = 1
+)
+# fit3
+saveRDS(fit3, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit3_mod_prenatal.RDS")))
+
+
+# --- Model reshist_addr1_no2_2016_aavg + cov. + mod. -----------------------------
+
+fit4 <- brm(
+  formula = bf(Harmonics_power1 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+                 pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power2 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power3 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power4 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power5 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)) +
+    bf(Harmonics_power6 ~ reshist_addr1_no2_2016_aavg*reshist_addr1_adi_quint + demo_sex_v2 + 
+         pubertal_dev_score + interview_age + race_ethnicity + birth_weight_kg + (1|site)),
+  data = data,
+  family = gaussian(),
+  cores = 8,
+  seed = 1
+)
+# fit4
+saveRDS(fit4, file = file.path(models_path, paste0(metric,"_",pollutant,"_fit4_mod_current.RDS")))
